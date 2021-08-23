196366ac85bd69baa263fec10ce470c35b6180e5
kgilbert78 <80863489+kgilbert78@users.noreply.github.com>
2021-08-22 22:29:25 -0400

    module 11 homework - add Auth0

--- server.js
+++ server.js
@@ -2,6 +2,33 @@ const server = require("express")();
 server.use(require("body-parser").json());
 server.use(require("cors")());
 
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+
+const auth0Domain = 'https://dev-oh-yzpnl.us.auth0.com';
+
+const checkPermissions = (permissions) => {
+  return jwtAuthz(permissions, {
+    customScopeKey: "permissions",
+    checkAllScopes: true,
+    failWithError: false
+  });
+};
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${auth0Domain}/.well-known/jwks.json`
+  }),
+
+  audience: `https://walkumentary-syracuse-backend.herokuapp.com/`,
+  issuer: [`${auth0Domain}/`],
+  algorithms: ['RS256']
+});
+
+
 // const googleMapsAPIKey = require("../apiKeys/googleMapsAPIKey");
 
 const {db, Site, Location, Photo, SoundEffect, TextCredit} = require("./models/db");
@@ -10,14 +37,14 @@ server.get("/", (req, res) => {
     res.send({hello: "World!"});
 });
 
-server.get("/sites/", async (req,res) => {
+server.get("/sites/", checkJwt, async (req,res) => {
     let siteData = await Site.findAll({
         include: [ {model: Location} ]
     });
     res.send({siteData});
 });
 
-server.get("/sites/:id", async (req,res) => {
+server.get("/sites/:id", checkJwt, async (req,res) => {
     let selectedSiteData = await Site.findAll({
         where: {siteID: req.params.id},
         include: [
